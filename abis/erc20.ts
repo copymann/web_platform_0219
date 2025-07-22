b34bc3cad33eb5d794e5942c52fd72710d846398
copymann <129855992+copymann@users.noreply.github.com>
2025-07-22 11:53:35 +0530

    Create erc20.ts,event listening logic
    event listening logic

---
+++ abis/erc20.ts
@@ -0,0 +1,20 @@
+export const erc20ABI = [
+  // A minimalist ABI for ERC20 tokens
+  {
+    "constant": true,
+    "inputs": [{ "name": "_owner", "type": "address" }],
+    "name": "balanceOf",
+    "outputs": [{ "name": "balance", "type": "uint256" }],
+    "type": "function"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": true, "name": "from", "type": "address" },
+      { "indexed": true, "name": "to", "type": "address" },
+      { "indexed": false, "name": "value", "type": "uint256" }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  }
+];
